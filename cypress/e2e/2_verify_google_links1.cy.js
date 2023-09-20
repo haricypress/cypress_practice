d15c371d4e516a2f266dab45ba4b8b42b0721f2a
haricypress <pirate.evil.war@gmail.com>
2023-09-20 20:47:24 +0530

    d

--- cypress/e2e/2_verify_google_links1.cy.js
+++ cypress/e2e/2_verify_google_links1.cy.js
@@ -1,24 +1,46 @@
 describe('Find links on web page', () => {
 
-    it('verify active links without open', () => {
-        var activel = 0
-        
-        cy.visit('https://www.google.com/search?q=cypress')
+    var url = 'https://www.google.com/search?q=cypress'  // google has infinity scroll down
 
+// =============================================================================================================
+
+it.only('verify working and not working links without open, write on file', () => {
+
+        var workingl = 0
+        var brockenl = 0
+
+        cy.visit(url)
         cy.get('a').each(($link, index) => {
-            const href = $link.attr('href')
+
+            const href = $link.prop('href')
+
             if (href) {
-                cy.request({ url: href, failOnStatusCode: false }).then((response) => {
+                cy.request({ url: href, failOnStatusCode: false}).then((response) => {
                     // failOnStatusCode: false    :  if brocken links are there skip Error
                     if (response.status < 400) {  // checking status
-                       
-                        cy.log(`link :  ${index + 1} is Active`)
-                        activel++
+
+                        // cy.log(`link :  ${index + 1} is working`)
+                        workingl++
+                        // cy.writeFile('cypress/fixtures/output/working_links.json', href + '\n', { flag: 'a+' })
 
                     } // if(response.status)
+                    else {
+                        // cy.log(`link :  ${index + 1} is brocken`)
+                        brockenl++
+                        // cy.writeFile('cypress/fixtures/output/brocken_links.json', href + '\n', { flag: 'a+' })
+                    } //else
                 })// cy.request
             } //if(href)
+        }).then(($link) => {
+            const totalLinks = $link.length
+            cy.log(` total links   :  ${totalLinks}`)
+            cy.log(`working links    :  ${workingl}`)
+            cy.log(`not working links  :  ${brockenl}`)
+
         })// cy.get('a') 
-        cy.log(`total active links :  ${activel}`)
+
     })  //it 
+// =============================================================================================================
+
 }) //describe
+
