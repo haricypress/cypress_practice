b40420bece289e4577097fe1521e71bb02cdafe4
haricypress <pirate.evil.war@gmail.com>
2023-09-20 16:32:50 +0530

    s

--- cypress/e2e/2_verify_google_links1.cy.js
+++ cypress/e2e/2_verify_google_links1.cy.js
@@ -1,16 +1,17 @@
-describe('Find broken links on the page', () => {
+describe('Find links on web page', () => {
 
-    it('verify navigation across the pages', () => {
+    it('verify active links without open', () => {
         var activel = 0
+        var links = ''
         cy.visit('https://www.google.com/search?q=cypress')
 
         cy.get('a').each(($link, index) => {
             const href = $link.attr('href')
             if (href) {
-                cy.request({ url: href, failOnStatusCode: false }).then((response) => {  
+                cy.request({ url: href, failOnStatusCode: false }).then((response) => {
                     // failOnStatusCode: false    :  if brocken links are there skip Error
-                    if (!response.status >= 400) {  // checking status
-
+                    if (response.status >= 400) {  // checking status
+                       
                         cy.log(`link :  ${index + 1} is Active`)
                         activel++
 
@@ -18,6 +19,6 @@ describe('Find broken links on the page', () => {
                 })// cy.request
             } //if(href)
         })// cy.get('a') 
-        cy.log(`total active links :  ${activel}`) 
+        cy.log(`total active links :  ${activel}`)
     })  //it 
 }) //describe
