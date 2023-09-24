ec6470230111e4b7651f7de7acec56ebf31324c2
haricypress <pirate.evil.war@gmail.com>
2023-09-24 18:07:55 +0530

    ss

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+
+  env: {
+    'username': 'Admin',
+  },
+
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
