ef74438689483e323519033a0091a22a535ca986
haricypress <pirate.evil.war@gmail.com>
2023-09-30 00:06:27 +0530

    installed BDD

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
 
 
   e2e: {
-    
+    "specPattern": "**/*.cy.js,feature",
         setupNodeEvents(on, config) {
       // implement node event listeners here
     },
