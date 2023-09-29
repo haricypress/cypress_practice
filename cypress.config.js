e603448183efec9ffc72c005404a6340cd7c362e
haricypress <pirate.evil.war@gmail.com>
2023-09-30 00:01:03 +0530

    k

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,8 @@ module.exports = defineConfig({
 
 
   e2e: {
-    setupNodeEvents(on, config) {
+    
+        setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
