19e6e30818c73c9fff6b74bb528ea9b16d3e0828
haricypress <pirate.evil.war@gmail.com>
2023-10-01 01:35:16 +0530

    d

--- cypress/e2e/common/login.js
+++ cypress/e2e/common/login.js
@@ -2,10 +2,10 @@ import { Given, Then } from "cypress-cucumber-preprocessor/steps";
 
 Given("this is given syntax", () => {
 
-    cy.log('this is login file Given')
+    cy.log('this is common file Given')
 
 })
 Then("this is Then syntax", () => {
 
-    cy.log('this is login file Then')
+    cy.log('this is common file Then')
 })
