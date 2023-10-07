2a4ebd8d7ca07f4e30375881a6a734b679db971f
haricypress <pirate.evil.war@gmail.com>
2023-10-07 19:07:22 +0530

    mm

--- cypress/e2e/0/originDemo_1.cy.js
+++ cypress/e2e/0/originDemo_1.cy.js
@@ -1,7 +1,7 @@
 describe('cypress working with diff websites', () => {
 
 
-//note : external pugins like "xpath" not work in "origin()"
+    //note : external pugins like "xpath" not work in "origin()"
 
 
     it('1. opening two diff websites without origin()', () => {
@@ -18,26 +18,7 @@ describe('cypress working with diff websites', () => {
 
 
     })// =========================================================================================
-    it.only('2. opening two diff websites with origin, if 2nd url automatically redirecting', () => {
-        /*
-        1st url : https://www.wikipedia.org
-        2nd url : https://commons.wikimedia.org
-        if 2nd url automatically redirecting to  https://commons.wikimedia.org/wiki/Main_Page ( not by cypress)
-        */
-
-        cy.visit('https://www.wikipedia.org/');
-        cy.get('a').contains('Commons').click();
-        cy.origin('https://commons.wikimedia.org', () => {
-
-            // some URLs redirecting automatically, so, after new url open,
-            // checking that new url redirected or not
-            cy.url().should("eq", "https://commons.wikimedia.org/wiki/Main_Page")
-
-            cy.get('.mainpage-welcome-sitename').should('contain.text', 'Wikimedia Commons');
-
-        })  //  origin
-    })// ==========================================================================================    
-    it('3. opening two diff websites with origin, if 2nd url not automatically redirecting', () => {
+    it('2. opening two diff websites with origin, if 2nd url not automatically redirecting', () => {
         /*
         1st url : https://www.google.com
         2nd url : https://www.cypress.io
@@ -51,13 +32,36 @@ describe('cypress working with diff websites', () => {
         cy.contains("Cypress").should("be.visible").click()
         cy.origin("https://www.cypress.io", () => {
 
-            // some URLs redirecting automatically, so, after new url open,
-            // checking that new url redirected or not
+            // cy.url() getting current url
+            // checking 2nd url and current url are same or not
             cy.url().should("eq", "https://www.cypress.io")
 
-            cy.contains('With Cypress, you can e').should("be.visible")
-            
+            cy.contains('With Cypress, you can e').should("be.visible")  // checking assertion in 2nd url
+
         }) //  origin
     }) // ==================================================================================================
 
+    it.only('3. opening two diff websites with origin, if 2nd url automatically redirecting', () => {
+        /*
+        1st url : https://www.wikipedia.org
+        2nd url : https://commons.wikimedia.org
+        3rd url : https://commons.wikimedia.org/wiki/Main_Page
+
+        2nd url automatically redirecting to 3rd url, not by "cyress"  
+        */
+
+        cy.visit('https://www.wikipedia.org/');
+        cy.get('a').contains('Commons').click();
+        cy.origin('https://commons.wikimedia.org', () => {
+
+            // some URLs redirecting automatically,
+            // checking 2nd url redirected to 3rd url or not
+            cy.url().should("eq", "https://commons.wikimedia.org/wiki/Main_Page")
+
+            cy.get('.mainpage-welcome-sitename').should('contain.text', 'Wikimedia Commons'); // checking assertion in 3rd url
+
+        })  //  origin
+    })// ==========================================================================================    
+
 })// describe
+
