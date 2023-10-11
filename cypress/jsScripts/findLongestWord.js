a2be4810ee0fe8d8bfea39696ee446782af69736
haricypress <pirate.evil.war@gmail.com>
2023-10-11 13:02:02 +0530

    jhghj

--- cypress/jsScripts/findLongestWord.js
+++ cypress/jsScripts/findLongestWord.js
@@ -1,17 +1,20 @@
+
 function findLongestWord(str) {
   var strSplit = str.split(' ');
+  console.log(str, strSplit, strSplit.length)
   var longest_length = 0
   var longest_word = ''
 
-  for (var i = 0; i < strSplit.length; i++) {
+  for (let i = 0; i < strSplit.length; i++) {
     if (strSplit[i].length > longest_length) {
       longest_length = strSplit[i].length;
       longest_word = strSplit[i]
     }
   }
-  return 'longest word : ' + longest_word + ' ('+longest_length+ ')';
+  return 'longest word : ' + longest_word + ' (' + longest_length + ')';
 }
 
-
-string = 'i am an indian'
-console.log(findLongestWord(string))
+string_value = 'i am an indian'
+string_empty = ''
+console.log(findLongestWord(string_value))
+console.log(findLongestWord(string_empty))
