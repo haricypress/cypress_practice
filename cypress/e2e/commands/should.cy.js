ba7ec2098da51925bb8985419b65b77d248bda1d
haricypress <pirate.evil.war@gmail.com>
2023-10-12 22:48:53 +0530

    s

--- cypress/e2e/commands/should.cy.js
+++ cypress/e2e/commands/should.cy.js
@@ -4,13 +4,17 @@ describe('Automation - Working With Links', () => {
         cy.visit('https://commons.wikimedia.org/wiki/Main_Page')
 
         // verifying links count
-        cy.get('a').should('have.length', 692)
+        cy.get('a').should('have.length', 695)
 
+
+        // below assertion will success, if element not exists in web page
+        // below assertion will fail, if element exists in web page
+        cy.xpath('//div[@class="mainpage-welcwome-sitename"]').should('not.exist')
 
         // using "then()"
         cy.get('a').its('length').then((elelength) => {
             cy.log(elelength)
-            expect(elelength).to.equal(692)
+            expect(elelength).to.equal(695)
         })//  cy.get('a')
 
 
