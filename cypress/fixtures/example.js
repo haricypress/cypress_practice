c890a4e4a9686f3915e524abdf908048d3ac4812
haricypress <pirate.evil.war@gmail.com>
2023-10-14 23:13:27 +0530

    newly cypress installed

--- cypress/fixtures/example.js
+++
@@ -1,5 +0,0 @@
-// Instead of writing a response inline you can
-// use a fixture file's content.
-cy.fixture('example.json').then((comment) => {
-    cy.intercept('GET', 'comments/*', comment).as('getComment')
-  })
