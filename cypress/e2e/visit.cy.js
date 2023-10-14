75e70924635542d1c6f81f1cbdf278adf07404c7
haricypress <pirate.evil.war@gmail.com>
2023-10-15 00:28:33 +0530

    sas

--- cypress/e2e/visit.cy.js
+++ cypress/e2e/visit.cy.js
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('1. viewport',
+
+
+  () => {
+    it('1. samsung-s10 and (400,400)', () => {
+
+      cy.log("it use resolution from describe")
+      cy.visit('https://example.cypress.io').pause()
+      
+      cy.viewport("samsung-s10")
+      cy.visit('https://example.cypress.io').pause()
+    
+      cy.viewport(1000,400)
+      cy.visit('https://example.cypress.io')
+       
+    })
   })
-})
+
+
+  describe('2. viewport',
+  () => {
+    it('1. samsung-s10 and (400,400)', () => {
+
+      cy.visit('https://example.cypress.io')
+       
+    })
+  })
+
