8eccd4c1ac4ddeb916d6c5872a3107cffa64e245
haricypress <pirate.evil.war@gmail.com>
2023-10-15 01:02:02 +0530

    testing on diff resolutions / screen sizes

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight:800,
+  viewportWidth:700,
+
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
