6a403a8186fdefd861418eddfdda33e1109a9ebf
haricypress <pirate.evil.war@gmail.com>
2023-11-30 02:17:18 +0530

    this kwyword

--- cypress/e2e/commands/01_this.cy.js
+++ cypress/e2e/commands/01_this.cy.js
@@ -1,12 +1,60 @@
+//  ) "this" keyword declared within test-suite, but not within sub-block, "before" hook used
 
-describe("TS1", ()=>{
 
-    it("TC1", function() {
+// scenario 1 :  "before" declared as  "arrow" function
 
+
+describe("TS1", function () {
+    this.data = "a"
+    before("before", ()=>{
+        cy.log(this.data, typeof(this.data))
+        this.data = "before"
+        cy.log(this.data)
+    })
+    it("TC1", () => {
+        cy.log(this.data)
         this.data = 1
         cy.log(this.data)
-
     })
+    it("TC3", function () {
+        cy.log(this.data)
+        this.data = 3
+        cy.log(this.data)
+    })
+	    this.data = "b"
+	    it("TC2", () => {
+        cy.log(this.data)
+        this.data = 2
+        cy.log(this.data)
+    })
+    this.data = "c"
 })
 
 
+// scenario 2 :  "before" declared with  "function" keyword
+
+describe("TS1", function () {
+    this.data = "a"
+    before("before", function ()  {
+        cy.log(this.data, typeof(this.data))
+        this.data = "before"
+        cy.log(this.data)
+    })
+    it("TC1", () => {
+        cy.log(this.data)
+        this.data = 1
+        cy.log(this.data)
+    })
+	    this.data = "b"
+    it("TC3", function () {
+        cy.log(this.data)
+        this.data = 3
+        cy.log(this.data)
+    })
+	    it("TC2", () => {
+        cy.log(this.data)
+        this.data = 2
+        cy.log(this.data)
+    })
+    this.data = "c"
+})
